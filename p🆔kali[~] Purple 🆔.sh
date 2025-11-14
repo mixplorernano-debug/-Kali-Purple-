e1bf906d08956f949b6619261368c3693f3c3d64
mixplorernano-Termux <225081509+mixplorernano-debug@users.noreply.github.com>
2025-11-13 17:25:13 -0700

    bash    nethunter

---
+++ p🆔kali[~] Purple 🆔.sh
@@ -0,0 +1,9 @@
+bash
+   termux-setup-storage
+   pkg install wget
+   wget -O install-nethunter-termux https://offs.ec/2MceZWr
+   chmod +x install-nethunter-termux
+   ./install-nethunter-termux
+   
+   bash
+   nethunter
